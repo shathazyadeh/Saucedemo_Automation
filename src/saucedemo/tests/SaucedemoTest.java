e9cd209d53632cca7a8c37594e62811c4e56f182
shathazyadeh <shatha.zyadeh@gmail.com>
2026-09-01 09:08:41 +0300

    Update login test cases and add negative test for locked user

--- src/saucedemo/tests/SaucedemoTest.java
+++ src/saucedemo/tests/SaucedemoTest.java
@@ -10,13 +10,17 @@ public class SaucedemoTest {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().window().maximize();
-		//2. "logo" // التحقق من ظهور شعار الموقع وقراءة نصه
+		
+		// Verify logo display
+		
 		boolean logoDisplayed=driver.findElement(By.cssSelector("div.login_logo")).isDisplayed();
 		String logoText = driver.findElement(By.cssSelector("div.login_logo")).getText();
 
 		System.out.println("Logo is displayed: " + logoDisplayed);
 		System.out.println("Logo text is: " + logoText);
-	   //3.التحقق من ظهور حقول الإدخال وزر الدخول
+	   
+		//Verify input fields & login button display
+		
 		boolean usernameDisplayed = driver.findElement(By.id("user-name")).isDisplayed();
         boolean passwordDisplayed = driver.findElement(By.id("password")).isDisplayed();
         boolean loginButtonDisplayed = driver.findElement(By.id("login-button")).isDisplayed();
@@ -24,18 +28,35 @@ public class SaucedemoTest {
         System.out.println("Username field is displayed: " + usernameDisplayed);
         System.out.println("Password field is displayed: " + passwordDisplayed);
         System.out.println("Login button is displayed: " + loginButtonDisplayed);
-		//4 ادخال باس ويوزر صحيحات 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        
+     // Negative Test Case for locked_out_user
+        
+        driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        //5
+        driver.findElement(By.id("login-button")).click();
+    
+        // Verify error message
+        
+        boolean isErrorDisplayed = driver.findElement(By.cssSelector("h3[data-test='error']")).isDisplayed();
+        String errorText = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+
+        System.out.println("Locked user error displayed: " + isErrorDisplayed);
+        System.out.println("Error message text: " + errorText);
+        
+     //clear user name field
+        driver.findElement(By.id("user-name")).clear();
+        
+		//Enter standard user login
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");     
         driver.findElement(By.id("login-button")).click();
 
-        //6
-     // التحقق من نجاح تسجيل الدخول والانتقال لصفحة المنتجات
+        // Verify successful login and redirect to inventory page
+        
         String currentUrl = driver.getCurrentUrl();
         boolean loginSuccess = currentUrl.contains("inventory.html");
         System.out.println("Login successful and redirected to inventory: " + loginSuccess);
-     // إغلاق المتصفح
+   
+        // close browser
         driver.quit();
         
         
